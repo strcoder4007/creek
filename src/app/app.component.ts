efa237a568a42300e2ef026a7ab80ac91bc95118
Shubham Singh <mynameshubham@gmail.com>
2017-11-26 03:50:08 +0400

    lastfm api confusion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+//import { LastfmAPI } from 'lastfmapi';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -9,15 +11,36 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
+    authUrl: string = "http://www.last.fm/api/auth/?api_key=61e21b6df4d0b37c1d620cd3aeb4b261";
+
     constructor(private http: HttpClient) {
 
     }
 
 
     ngOnInit(): void {
-        this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
+
+        
+        this.http.get(this.authUrl).subscribe(data => {
             console.log(data);
+          });
+
+
+        /*
+        var lfm = new LastfmAPI({
+            'api_key': '61e21b6df4d0b37c1d620cd3aeb4b261',
+            'secret': '0f939f86d1ddf1ed8478273ba8576799'
         });
-      }
+
+        lfm.track.getInfo({
+            'artist' : 'Katatonia',
+            'track' : 'criminals'
+        }, function (err, track) {
+            if (err) { throw err; }
+            console.log(track);
+        });
+        */
+
+    }
 
 }
