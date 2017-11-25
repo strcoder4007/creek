37af723ac35d4940fe2a81767d710db15038af82
Shubham Singh <mynameshubham@gmail.com>
2017-11-26 02:28:40 +0400

    dependencies added, httpmodule working, leaving frontend for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+    constructor(private http: HttpClient) {
+
+    }
+
+
+    ngOnInit(): void {
+        this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
+            console.log(data);
+        });
+      }
+
 }
