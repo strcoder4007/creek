1d75c2d2cbef186be5b9f49e59d7ee5241213a73
Shubham Singh <mynameshubham@gmail.com>
2018-03-20 02:46:01 +0400

    filtered the new albums but its working very slow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,12 @@ import 'rxjs/Rx';
 })
 export class AppComponent implements OnInit {
 
+    sessionKey: String = "";
+    getTracks: any;
+    getArtists: any;
+    getAlbums: any;
+    myAlbums = [];
+
     constructor(private http: HttpClient) {
     }
 
@@ -21,12 +27,11 @@ export class AppComponent implements OnInit {
 
     processJson(url) {
         this.getposts(url).subscribe((data) => {
-            console.log(data['session']['key']);
+            this.sessionKey = data['session']['key'];
         })
     }
 
     ngOnInit(): void {
-        var sessionKey = "";
         var baseUrl = "http://localhost:4200/";
         //var baseUrl = "http://18.221.40.67/creek/";
         var apiKey = "bfbcadbcde949bde7890892ea8d9d698";
@@ -43,13 +48,43 @@ export class AppComponent implements OnInit {
             var authString = "api_key" + apiKey + "methodauth.getSessiontoken"+token+secret;
             var apiSig = Md5.hashStr(authString);
             let data = "http://ws.audioscrobbler.com/2.0/?method=auth.getSession&token=" + token + "&api_key=" + apiKey + "&api_sig=" + apiSig + "&format=json";
-            this.processJson(data);
+            this.getposts(data).subscribe((res) => {
+                this.sessionKey = res['session']['key'];
+            })
+
+            let getArtistsUrl = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=strgamer4007&api_key=" + apiKey + "&format=json";
+            this.getposts(getArtistsUrl).subscribe((res) => {
+                this.getArtists = res['topartists']['artist'];
+                for(let i = 0; i < this.getArtists.length; i++) {
+                    let getAlbumsUrl = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + this.getArtists[i].name + "&api_key=" + apiKey + "&format=json";
+                    this.getposts(getAlbumsUrl).subscribe((albumRes) => {
+                        if(albumRes != undefined) {
+                            this.getAlbums = albumRes['topalbums']['album'];
+                            for(let j = 0; j < this.getAlbums.length; j++) {
+                                let getAlbumInfoUrl = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" + apiKey + "&artist=" + this.getArtists[i].name + "&album=" + this.getAlbums[j].name + "&format=json";
+                                this.getposts(getAlbumInfoUrl).subscribe((albumInfoRes) => {
+                                    if(albumInfoRes['album'] != undefined) {
+                                        if(albumInfoRes['album'].wiki != undefined) {
+                                            let junk = albumInfoRes['album'].wiki.published;
+                                            if(parseInt(junk.split(' ')[2]) >= 2017) {
+                                                this.myAlbums.push(albumInfoRes['album']);
+                                                console.log("selected album: ", albumInfoRes['album']);
+                                            }
+                                        }
+                                    }
+                                })
+                            }
+                        }
+                    })
+                }
+                
+            })
+
         }
 
     }
 
 }
-
 
 /*
 Application name 	creek
